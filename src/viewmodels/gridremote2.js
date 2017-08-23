a599da9ca0e2f7bb3ebd6f43d157ebc5362aa846
Daniel <danelebash@hotmail.com>
2017-08-23 15:42:59 -0700

    Initial commit

---
+++ src/viewmodels/gridremote2.js
@@ -0,0 +1,55 @@
+import {inject} from 'aurelia-framework';
+import {LoopBackAdapter} from '../services/syncfusion-dreamfactory-adaptor';
+import {AuthenticationService} from '../services/auth-service'
+import dfconfig from '../config/dreamfactory-config'
+
+@inject(AuthenticationService,LoopBackAdapter)
+export class GridQuery {
+  constructor(authservice,adaptor) {
+    this.authservice = authservice
+    this.adaptor = adaptor
+  }
+
+  attached() {
+    if (this.authservice.authenticated === false) {
+      alert('please login')
+    } else {
+      this.getdata();
+    }
+  }
+
+  getdata() {
+    //requestType = "get" -- request uses query string params via get, "json" -- request uses post to send an object
+    let adaptorOptions = {requestType: "json"}; //defaults to "get" if not specified or not passed in
+
+    let dataManager = ej.DataManager({
+      url: "http://localhost:3000/api/customers",
+      adaptor: new this.adaptor.syncfusionDreamFactoryAdaptor(adaptorOptions),
+    });
+
+    $("#Grid").ejGrid({
+      dataSource: dataManager,
+      toolbarSettings: {
+        showToolbar: true,
+        toolbarItems: ["add", "edit", "delete"]
+      },
+      editSettings: {
+        allowEditing: true,
+        allowAdding: true,
+        allowDeleting: true,
+        editMode: "dialog"
+      },
+      allowPaging: true,
+      allowSorting: true,
+      allowFiltering: true,
+      filterSettings: {showPredicate: true, filterType: "menu", enableCaseSensitivity: true},
+      searchSettings: {ignoreCase: false},
+      isResponsive: true,
+      columns: [
+        {field: "lastName", headerText: "Last Name", width: 110},
+        {field: "company", headerText: "Company", width: 110}
+      ]
+    });
+  }
+}
+
